fc9872efa8acd07d2cdb3dfc0707c5c22b204a9d
vKarich2 <116518105+vKarich2@users.noreply.github.com>
2023-01-09 22:12:58 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -57,7 +57,7 @@ function loadTrack(track_index){
 	reset();
 	curr_track.src = music_list[track_index].music;
 	curr_track.load();
-	track_art.style.backgroundImage = 'url(../' + music_list[track_index].img + ')';
+	track_art.style.backgroundImage = 'url(./' + music_list[track_index].img + ')';
 	track_name.textContent = music_list[track_index].name;
 	track_artist.textContent = music_list[track_index].artist;
 	now_playing.textContent = 'Playing music ' + (track_index + 1) + ' of ' + music_list.length;
